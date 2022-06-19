7dee587da8f08e84f717a9f12459e560168bafd8
sarijk <jatuksari@gmail.com>
2022-06-19 18:08:17 +0700

    commit

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 const books = [];
 const RENDER_EVENT = 'render-book';
 
-document.addEventListener(RENDER_EVENT, function() {
-    console.log(books);
-});
-
 function generateID() {
     return +new Date();
 }
@@ -19,10 +15,33 @@ function generateID() {
     }
 }
 
-function dataBook() {
+function makeData(bookObject) {
+    const {id, title, author, year, isCompleted} = bookObject;
+
+    const textTitle = document.createElement("h3");
+    textTitle.innerText = title;
+
+    const textTimestamp = document.createElement("h4");
+    textTimestamp.innerText = author
+
+    const textAuthor = document.createElement("p");
+    textAuthor.innerText = year;
+
+    const dataCard = document.createElement("div");
+    dataCard.classList.add("card");
+    dataCard.append(textTitle, textTimestamp, textAuthor);
+
+    const container = document.createElement("div");
+    container.classList.add("container");
+    container.append(dataCard);
+    container.setAttribute("id", `book-${id}`);
+}
+
+function addBook() {
     const title = document.getElementById("title").value;
     const author = document.getElementById("author").value;
     const year = document.getElementById("year").value;
+
     const generateID = generateID()
     const inputData = generateInputObject(generateID, title, author, year, false)
 
@@ -39,3 +58,8 @@ document.addEventListener('DOMContentLoaded', function() {
         addBook();
     });
 });
+
+document.addEventListener(RENDER_EVENT, function() {
+    console.log(books);
+});
+
